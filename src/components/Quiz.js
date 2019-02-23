cbf4624ab832e22c05e86e7e71d8389382b0644f
Joshua Lavarine <joshua@lavarine.com>
2019-02-23 15:04:24 -0700

    Display possible answers (correct and incorrect) in Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -21,14 +21,49 @@ class Quiz extends Component {
       return question.id == this.state.currentQuestion
     })
     return (
-      <p>Question: {currentQuestion.prompt}</p>
+      <section>
+        <p>Question: {currentQuestion.prompt}</p>
+        <p>Possible Answers:</p>
+        {/* <p>{currentQuestion.correctAnswer}</p> */}
+        {/* <p>{incorrectChoices}</p> */}
+      </section>
+    )
+  }
+
+  displayPossibleAnswers = () => {
+    let selectedPioneer = this.props.pioneers.pioneersData.find(pioneer => {
+      return pioneer.id == this.props.currentPioneer
+    })
+    let currentQuestion = selectedPioneer.multipleChoice.questions.find(question => {
+      return question.id == this.state.currentQuestion
+    })
+    let incorrectChoices = currentQuestion.incorrectAnswers.map(question => {
+      return (
+        <p>{question}</p>
+      )
+    })
+    return incorrectChoices
+  }
+
+  displayCorrectAnswer = () => {
+    let selectedPioneer = this.props.pioneers.pioneersData.find(pioneer => {
+      return pioneer.id == this.props.currentPioneer
+    })
+    let currentQuestion = selectedPioneer.multipleChoice.questions.find(question => {
+      return question.id == this.state.currentQuestion
+    })
+    let correctAnswer = currentQuestion.correctAnswer
+    return (
+      <p>{correctAnswer}</p>
     )
   }
 
   render() {
     return (
       <form>
-        <div>{this.displayPrompt()}</div>
+        <section>{this.displayPrompt()}</section>
+        <section>{this.displayCorrectAnswer()}</section>
+        <section>{this.displayPossibleAnswers()}</section>
       </form>
     )
   }
