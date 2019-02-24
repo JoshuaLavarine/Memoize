e856d4f2212fa216c5325879afb277cd9a5dda6b
Joshua Lavarine <joshua@lavarine.com>
2019-02-23 17:52:16 -0700

    Check for correct answer in Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,7 +5,8 @@ class Quiz extends Component {
     super();
     this.state = {
       currentQuestion: 1,
-      guessesCorrect: 0
+      guessesCorrect: 0,
+      guessValue: ''
     }
   }
 
@@ -24,8 +25,6 @@ class Quiz extends Component {
       <section>
         <p>Question: {currentQuestion.prompt}</p>
         <p>Possible Answers:</p>
-        {/* <p>{currentQuestion.correctAnswer}</p> */}
-        {/* <p>{incorrectChoices}</p> */}
       </section>
     )
   }
@@ -39,7 +38,10 @@ class Quiz extends Component {
     })
     let incorrectChoices = currentQuestion.incorrectAnswers.map(question => {
       return (
-        <p>{question}</p>
+        <section>
+          <input type="radio" id={Date.now()} value={question}></input>
+          <label for={Date.now()}>{question}</label>
+        </section>
       )
     })
     return incorrectChoices
@@ -54,16 +56,49 @@ class Quiz extends Component {
     })
     let correctAnswer = currentQuestion.correctAnswer
     return (
-      <p>{correctAnswer}</p>
+      <section>
+      <input type="radio" id={Date.now()} value={correctAnswer}></input>
+      <label for={Date.now()}>{correctAnswer}</label>
+      </section>
     )
+  }
+
+  getClickedValue = (event) => {
+    this.setState({
+      guessValue: event.target.value
+    })
+  }
+
+  checkCorrectAnswer = (e) => {
+    e.preventDefault();
+    let selectedPioneer = this.props.pioneers.pioneersData.find(pioneer => {
+      return pioneer.id == this.props.currentPioneer
+    })
+    let currentQuestion = selectedPioneer.multipleChoice.questions.find(question => {
+      return question.id == this.state.currentQuestion
+    })
+    let correctAnswer = currentQuestion.correctAnswer
+    if (this.state.guessValue === correctAnswer) {
+      this.setState({
+        guessesCorrect: this.state.guessesCorrect + 1,
+        currentQuestion: this.state.currentQuestion + 1,
+        guessValue: ''
+      })
+    } else {
+      this.setState({
+        currentQuestion: this.state.currentQuestion + 1,
+        guessValue: ''
+      })
+    }
   }
 
   render() {
     return (
       <form>
-        <section>{this.displayPrompt()}</section>
-        <section>{this.displayCorrectAnswer()}</section>
-        <section>{this.displayPossibleAnswers()}</section>
+        <section onClick={this.getClickedValue}>{this.displayPrompt()}</section>
+        <section onClick={this.getClickedValue}>{this.displayCorrectAnswer()}</section>
+        <section onClick={this.getClickedValue}>{this.displayPossibleAnswers()}</section>
+        <button type="reset" onClick={this.checkCorrectAnswer}>Submit Answer</button>
       </form>
     )
   }
