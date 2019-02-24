d5b152c87c1216f082e0463d9ad82754bfeec492
Joshua Lavarine <joshua@lavarine.com>
2019-02-23 23:08:56 -0700

    Remove local storage from all components

--- src/components/App.js
+++ src/components/App.js
@@ -11,63 +11,19 @@ class App extends Component {
     }
   }
 
-  //   fetch("http://")
-  //     .then(response => response.json())
-  //     .then(data => {
-    //       this.setState({
-      //         pioneers: data.pioneers
-      //       });
-      //     })
-      //     .catch(error => {
-        //       throw new Error(error);
-        //     }); 
-        
-  componentDidMount() {
-    this.hydrateStateWithLocalStorage();
-    // add event listener to save state to localStorage
-    // when user leaves/refreshes the page
-    window.addEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage
-    );
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage
-    );
-    // saves if component has a chance to unmount
-    this.saveStateToLocalStorage();
-}
-
-
-  hydrateStateWithLocalStorage() {
-    // for all items in state
-    for (let key in this.state) {
-      // if the key exists in localStorage
-      if (localStorage.hasOwnProperty(key)) {
-        // get the key's value from localStorage
-        let value = localStorage.getItem(key);
-        // parse the localStorage string and setState
-        try {
-          value = JSON.parse(value);
-          this.setState({ [key]: value });
-        } catch (e) {
-          // handle empty string
-          this.setState({ [key]: value });
-        }
-      }
-    }
-  }
-
-  saveStateToLocalStorage = () => {
-    // for every item in React state
-    for (let key in this.state) {
-      // save to localStorage
-      localStorage.setItem(key, JSON.stringify(this.state[key]));
-    }
-  }
+  
+  // componentDidMount() {
+    //   fetch("http://")
+    //     .then(response => response.json())
+    //     .then(data => {
+      //       this.setState({
+        //         pioneers: data.pioneers
+        //       });
+        //     })
+        //     .catch(error => {
+          //       throw new Error(error);
+          //     }); 
+  // }
 
   render() {
     return (
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -13,53 +13,6 @@ class Quiz extends Component {
     }
   }
 
-  componentDidMount() {
-    this.hydrateStateWithLocalStorage();
-    // add event listener to save state to localStorage
-    // when user leaves/refreshes the page
-    window.addEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage
-    );
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener(
-      "beforeunload",
-      this.saveStateToLocalStorage
-    );
-    // saves if component has a chance to unmount
-    this.saveStateToLocalStorage();
-}
-
-
-  hydrateStateWithLocalStorage() {
-    // for all items in state
-    for (let key in this.state) {
-      // if the key exists in localStorage
-      if (localStorage.hasOwnProperty(key)) {
-        // get the key's value from localStorage
-        let value = localStorage.getItem(key);
-        // parse the localStorage string and setState
-        try {
-          value = JSON.parse(value);
-          this.setState({ [key]: value });
-        } catch (e) {
-          // handle empty string
-          this.setState({ [key]: value });
-        }
-      }
-    }
-  }
-
-  saveStateToLocalStorage = () => {
-    // for every item in React state
-    for (let key in this.state) {
-      // save to localStorage
-      localStorage.setItem(key, JSON.stringify(this.state[key]));
-    }
-  }
-
   makeGuess = () => {
   }
 
